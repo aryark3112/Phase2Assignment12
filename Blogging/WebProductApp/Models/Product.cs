ca4baa12623c363a3c5ac135ee1f66fd8ea591bc
aryark <aryark3112@gmail.com>
2023-08-28 16:24:04 +0530

    blogging

---
+++ Blogging/WebProductApp/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebProductApp.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        public int PId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Company { get; set; }
+    }
+}
